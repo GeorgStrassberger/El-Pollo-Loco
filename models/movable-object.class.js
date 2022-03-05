99683787665cc7594c289749bec8cc549da20213
Georg <georg.strassberger@googlemail.com>
2022-03-05 16:38:19 +0100

    object classes added

---
+++ models/movable-object.class.js
@@ -0,0 +1,21 @@
+class MovableObject {
+    x = 120;
+    y = 250;
+    img;
+    height = 150;
+    width = 100;
+
+
+    loadImage(path) { // loadImage('img/test.png')
+        this.img = new Image(); // this.img = document.getElementById('Image') <img id="image">
+        this.img.src = path;
+    }
+
+    moveRight() {
+        console.log('Moving right');
+    }
+
+    moveLeft() {
+        console.log('Moving left');
+    }
+}
